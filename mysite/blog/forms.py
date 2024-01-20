53fc988a60ea45f3f10767a274f2c86f33482b45
sergiu porime <126587603+sergiu19996@users.noreply.github.com>
2024-01-20 13:30:15 +0000

    Update forms.py

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,10 +1,6 @@
 from django import forms
 from .models import Comment
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['name', 'email', 'body']
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -12,6 +8,13 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False,
                                widget=forms.Textarea)
-    
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
+
+
 class SearchForm(forms.Form):
     query = forms.CharField()
